4cb111ba9266fb8a314fdf39f70aa3626dbca10d
rezaLin <rezanej666@gmail.com>
2023-08-06 23:10:29 +0330

    Do exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitedStr=str.split("");
+    let reversedArray=[];
+    for (let i = splitedStr.length-1; i >=0 ; i--) {
+        reversedArray.push(splitedStr[i]);
+    }
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
