d6749cbfe38c5b1b4e676e4e17c92472cfc22b3d
rezaLin <rezanej666@gmail.com>
2023-08-06 23:27:08 +0330

    Do exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array,...remove) {
+    for (const object of remove) {
+        for (let i = 0; i < array.length; i++) {
+            if(object===array[i])
+            {
+                array.splice(i,1);
+            }
+            
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
