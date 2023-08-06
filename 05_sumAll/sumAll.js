ff2b38322a7f7f6d0c08e252c9c561659d48259e
rezaLin <rezanej666@gmail.com>
2023-08-06 23:37:29 +0330

    Do exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(from ,to) {
+    if(from<0 || to <0 || typeof(from)!=="number"|| typeof(to)!=="number")
+        return "ERROR"
+    if (from>to)
+    {
+        let temp=from;
+        from=to;
+        to=temp;
+    }
+    let sum=from;
+    for (let i = from+1; i <= to; i++) {
+        sum+=i;
+        
+    }
+    return sum;
 };
 
 // Do not edit below this line
