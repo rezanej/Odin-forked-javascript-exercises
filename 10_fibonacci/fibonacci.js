99b0e5f98a72fd222908499628fa6e842615fc56
rezaLin <rezanej666@gmail.com>
2023-08-08 13:06:27 +0330

    Do exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0)
+        return "OOPS";
+    let a=1;
+    let b=1;
+    for(let i=0;i<num-2;i++)
+    {
+        let temp=a;
+        a=b;
+        b=temp+b;
+    }
+    return b;
 };
 
 // Do not edit below this line
