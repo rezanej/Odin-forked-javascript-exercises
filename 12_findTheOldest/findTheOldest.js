15fc0adbbf2f681e3d303d28fd9aa9a6be103aba
rezaLin <rezanej666@gmail.com>
2023-08-08 13:42:27 +0330

    Do exercise 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr=arr.sort((a,b)=>{
+         return (b.yearOfBirth)-(a.yearOfBirth);
+    });
+    arr=arr.sort((a,b)=>{
+        return (a.yearOfDeath-a.yearOfBirth)-(b.yearOfDeath-b.yearOfBirth);
+    });
+    console.log(arr);
+    return arr[arr.length-1];
 };
 
 // Do not edit below this line
