0aa5462e265388be56982c823c1841415292153d
rezaLin <rezanej666@gmail.com>
2023-08-08 12:42:31 +0330

    doing additionals

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+	return num1+num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1-num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let s=0;
+  for (let index = 0; index < arr.length; index++) {
+    s+=arr[index];
+  }
+  return s;
 };
 
-const multiply = function() {
+const multiply = function(nums) {
+  let mul=nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    mul*=nums[i];
+    
+  }
+  return mul;
 
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return Math.pow(num1,num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let f=1;
+	for (let i = 2; i <= num; i++) {
+    f*=i;
+  }
+  return f;
 };
 
 // Do not edit below this line
