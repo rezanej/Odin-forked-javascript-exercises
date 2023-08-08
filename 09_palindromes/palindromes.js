39967f01e429658e9299189f582ac14e0e82abe2
rezaLin <rezanej666@gmail.com>
2023-08-08 13:01:29 +0330

    Do exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let chars=str.trim().toLowerCase().split("");
+    chars=chars.filter((char)=>{
+        if(char!==' ' && char!=='.'&& char!=='!' && char!=='?' && char!==',')
+        return true;
 
+    }
+        );
+    let revChars=[];
+    for (let index =chars.length-1 ; index >=0; index--) {
+        revChars.push(chars[index]);
+        
+    }
+    return chars.join("")===revChars.join("");
 };
 
 // Do not edit below this line
